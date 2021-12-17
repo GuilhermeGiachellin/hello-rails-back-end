ec439404e8b4a9a7ac9a3466df4dfd3099fa5f74
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-12-17 17:11:37 -0300

    Add linter and fix linter errors

--- app/controllers/v1/random_messages_controller.rb
+++ app/controllers/v1/random_messages_controller.rb
@@ -1,6 +1,6 @@
 class V1::RandomMessagesController < ApplicationController
-    def show
-      @random = Message.order('RANDOM()').limit(1)
-      render json: { data: { id: @random[0].id, message: @random[0].name } }
-    end
+  def show
+    @random = Message.order('RANDOM()').limit(1)
+    render json: { data: { id: @random[0].id, message: @random[0].name } }
   end
+end
